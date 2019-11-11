5b7c6b2330b764986d75c62dc9e597d0e4605b88
mikejeter <mjeter45@yahoo.com>
2019-11-10 20:10:51 -0500

    Created Display.js

---
+++ src/components/Display.js
@@ -0,0 +1,12 @@
+mport React from "react"
+
+const Display = ({strikes, balls}) => {
+    return (
+        <div className="display">
+            <h2>Display</h2>
+            <p>Strike: {strikes}  Balls: {balls}</p>
+        </div>
+    )
+}
+
+export default Display;
