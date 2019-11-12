b63dacd7d9d49fba03f18c795810a0ba9953ebfd
mikejeter <mjeter45@yahoo.com>
2019-11-11 20:51:09 -0500

    Modified App.js, App.css, App.test.js, Display.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import * as rtl from '@testing-library/react';
+
+afterEach(rtl.cleanup)
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const header= rtl.render(
+    <h1>Board</h1>
+    // <button>Strike</button>
+  )
+  const element = header.queryByText(/board/i)
 });
